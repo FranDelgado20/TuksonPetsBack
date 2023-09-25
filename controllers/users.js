406164e2875b80f105dea1d7b52fbfa7c12d4287
SantiJaime <santij03@gmail.com>
2023-09-25 16:25:19 -0300

    actualizo controladores y rutas de users

--- controllers/users.js
+++ controllers/users.js
@@ -1,8 +1,10 @@
 const UserModel = require("../models/user");
+const { validationResult } = require("express-validator");
+const bcrypt = require("bcryptjs");
 
 const getAllUsers = async (req, res) => {
   try {
-    const allUsers = await UserModel.find()
+    const allUsers = await UserModel.find();
     res.status(200).json({ msg: "Usuarios encontrados", allUsers });
   } catch (error) {
     res
@@ -11,28 +13,71 @@ const getAllUsers = async (req, res) => {
   }
 };
 const getOneUser = async (req, res) => {
+  const errors = validationResult(req);
+
+  if (!errors.isEmpty()) {
+    return res.status(422).json({ msg: errors.array() });
+  }
   try {
-    res.status(200).json({ msg: "Usuaris encontrado" });
+    const oneUser = await UserModel.findOne({ _id: req.params.id });
+    res.status(200).json({ msg: "Usuaris encontrado", oneUser });
   } catch (error) {
     res.status(500).json({ msg: "No se pudo encontrar el usuario", error });
   }
 };
 const createUser = async (req, res) => {
+  const errors = validationResult(req);
+
+  if (!errors.isEmpty()) {
+    return res.status(422).json({ msg: errors.array() });
+  }
   try {
-    res.status(200).json({ msg: "Usuario creado correctamente" });
+    const emailExist = await UserModel.findOne({ email: req.body.email });
+
+    if (emailExist) {
+      return res
+        .status(422)
+        .json({ msg: "El Email ya se encuentra registrado" });
+    }
+
+    const newUser = new UserModel(req.body);
+
+    const salt = bcrypt.genSaltSync();
+    newUser.pass = await bcrypt.hash(req.body.pass, salt);
+
+    await newUser.save();
+
+    res.status(200).json({ msg: "Usuario creado correctamente", newUser });
   } catch (error) {
     res.status(500).json({ msg: "No se pudo crear el usuario", error });
   }
 };
 const updateUser = async (req, res) => {
+  const errors = validationResult(req);
+
+  if (!errors.isEmpty()) {
+    return res.status(422).json({ msg: errors.array() });
+  }
   try {
-    res.status(200).json({ msg: "Usuario editado correctamente" });
+    const updatedUser = await UserModel.findByIdAndUpdate(
+      { _id: req.params.id },
+      req.body,
+      { new: true }
+    );
+    res.status(200).json({ msg: "Usuario editado correctamente", updatedUser });
   } catch (error) {
     res.status(500).json({ msg: "No se pudo editar el usuario", error });
   }
 };
 const deleteUser = async (req, res) => {
+  const errors = validationResult(req);
+
+  if (!errors.isEmpty()) {
+    return res.status(422).json({ msg: errors.array() });
+  }
   try {
+    await UserModel.findByIdAndDelete({ _id: req.params.id });
+
     res.status(200).json({ msg: "Usuario eliminado correctamente" });
   } catch (error) {
     res.status(500).json({ msg: "No se pudo eliminar el usuario", error });
