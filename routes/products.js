e53ebc4600cab837b9e256f1d140f7e7feca4a0e
SantiJaime <santij03@gmail.com>
2023-09-26 15:04:52 -0300

    Añado middleware de autorización para limitar los permisos de los usuarios

--- routes/products.js
+++ routes/products.js
@@ -1,28 +1,51 @@
-const express = require('express')
-const { getAllProducts, getOneProducts, createProduct, editProduct, deleteProduct } = require('../controllers/products')
-const {check} = require('express-validator')
-const router = express.Router()
+const express = require("express");
+const {
+  getAllProducts,
+  getOneProducts,
+  createProduct,
+  editProduct,
+  deleteProduct,
+} = require("../controllers/products");
+const { check } = require("express-validator");
+const auth = require("../middleware/auth");
+const router = express.Router();
 
-router.get('/', getAllProducts)
+router.get("/", getAllProducts);
 
-router.get('/:id', [
-    check('id', 'Formato ID incorrecto').isMongoId()], getOneProducts)
+router.get(
+  "/:id",
+  [check("id", "Formato ID incorrecto").isMongoId()],
+  getOneProducts
+);
 
-router.post('/', [
+router.post(
+  "/",
+  [
     check("nombre", "El campo nombre esta vacio").notEmpty(),
-    check('nombre', 'Min 3 caracteres, Max de 50 caracteres').isLength({min:3, max:50}),
+    check("nombre", "Min 3 caracteres, Max de 50 caracteres").isLength({
+      min: 3,
+      max: 50,
+    }),
     check("descripcion", "El campo descripcion esta vacio").notEmpty(),
     check("categoria", "El campo categoria esta vacio").notEmpty(),
     check("imagen", "El campo imagen esta vacio").notEmpty(),
-    check('precio',' El campo precio esta vacio').notEmpty()
-],createProduct)
+    check("precio", " El campo precio esta vacio").notEmpty(),
+  ],
+  createProduct
+);
 
-router.put('/:id', [
-    check('id', 'Formato ID incorrecto').isMongoId()
-],editProduct)
+router.put(
+  "/:id",
+  [check("id", "Formato ID incorrecto").isMongoId()],
+  auth("admin"),
+  editProduct
+);
 
-router.delete('/:id',[
-    check('id', 'Formato ID incorrecto').isMongoId()
-] ,deleteProduct)
+router.delete(
+  "/:id",
+  [check("id", "Formato ID incorrecto").isMongoId()],
+  auth("admin"),
+  deleteProduct
+);
 
-module.exports = router
+module.exports = router;
