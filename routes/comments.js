ed136e951e5abb90bfaaa5edf0919a8026fe1fc6
Francisco Delgado <delgadofran78@gmail.com>
2023-09-27 14:45:22 -0300

    Creacion del modelo, rutas y controladores de Comentarios, modificacion en App.js

---
+++ routes/comments.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const { getAllComments, createComments } = require("../controllers/comments");
+const { check } = require("express-validator");
+const router = express.Router();
+
+router.get("/", getAllComments);
+router.post(
+  "/",
+  [
+    check("nombreApellido", "El campo nombre y apellido esta vacio").notEmpty(),
+    check("email", "El campo mail esta vacio").notEmpty(),
+    check("email", "Formato Email inválido").isEmail(),
+    check("mensaje", "El campo mensaje esta vacio").notEmpty(),
+    check("mensaje", "Min 15 caracteres, Max de 220 caracteres").isLength({
+      min: 15,
+      max: 220,
+    }),
+  ],
+  createComments
+);
+
+module.exports = router;
