97c451a88d83cf0abf58d331baa9cce35e58e6fe
SantiJaime <santij03@gmail.com>
2023-09-28 11:26:17 -0300

    Añado ruta, modelo y controladores de profesionales. Modifico app.js

---
+++ controllers/pros.js
@@ -0,0 +1,33 @@
+const { validationResult } = require("express-validator");
+const ProsModel = require("../models/pros");
+
+const getAllPros = async (req, res) => {
+  try {
+    const allPros = await ProsModel.find();
+    res.status(200).json({ msg: "Profesionales encontrados", allPros });
+  } catch (error) {
+    res
+      .status(500)
+      .json({ msg: "No se pudieron encontrar los profesionales", error });
+  }
+};
+
+const createPro = async (req, res) => {
+  const errors = validationResult(req);
+  if (!errors) {
+    return res.status(422).json({ msg: errors.array() });
+  }
+  try {
+    const newPro = new ProsModel(req.body);
+    await newPro.save();
+
+    res.status(201).json({ msg: "Profesional creado correctamente", newPro });
+  } catch (error) {
+    res.status(500).json({ msg: "No se pudo crear el profesional", error });
+  }
+};
+
+module.exports = {
+  getAllPros,
+  createPro,
+};
