4451a479fb7203cc3f280129d7a4ca10dbfe9e15
SantiJaime <santij03@gmail.com>
2023-10-02 21:57:00 -0300

    añado obtencion de productos con paginacion

--- controllers/products.js
+++ controllers/products.js
@@ -1,10 +1,32 @@
 const ModelProduct = require("../models/products");
 const { validationResult } = require("express-validator");
 
+const getAllProductsPagination = async (req, res) => {
+  try {
+    const to = Number(req.query.to) || 0;
+    const limit = Number(req.query.limit) || 4;
+
+    const [products, count] = await Promise.all([
+      ModelProduct.find()
+        .skip(to * limit)
+        .limit(limit),
+      ModelProduct.countDocuments(),
+    ]);
+    res
+      .status(200)
+      .json({ msg: "Productos encontrados", products, count, limit });
+  } catch (error) {
+    res
+      .status(500)
+      .json({ msg: "Hubo un error al obtener los productos", error });
+  }
+};
+
 const getAllProducts = async (req, res) => {
   try {
-    const getProducts = await ModelProduct.find();
-    res.status(200).json({ msg: "Productos encontrados", getProducts });
+    const allProducts = await ModelProduct.find();
+
+    res.status(200).json({ msg: "Productos encontrados", allProducts });
   } catch (error) {
     res
       .status(500)
@@ -12,11 +34,11 @@ const getAllProducts = async (req, res) => {
   }
 };
 const getOneProducts = async (req, res) => {
-    const errors = validationResult(req);
+  const errors = validationResult(req);
 
-    if (!errors.isEmpty()) {
-      return res.status(422).json({ msg: errors.array() });
-    }
+  if (!errors.isEmpty()) {
+    return res.status(422).json({ msg: errors.array() });
+  }
   try {
     const oneProduct = await ModelProduct.findOne({ _id: req.params.id });
     res.status(200).json({ msg: "Producto encontrado", oneProduct });
@@ -27,11 +49,11 @@ const getOneProducts = async (req, res) => {
   }
 };
 const createProduct = async (req, res) => {
-    const errors = validationResult(req);
+  const errors = validationResult(req);
 
-    if (!errors.isEmpty()) {
-      return res.status(422).json({ msg: errors.array() });
-    }
+  if (!errors.isEmpty()) {
+    return res.status(422).json({ msg: errors.array() });
+  }
   try {
     const { body } = req;
     const newProduct = new ModelProduct(body);
@@ -42,11 +64,11 @@ const createProduct = async (req, res) => {
   }
 };
 const editProduct = async (req, res) => {
-    const errors = validationResult(req);
+  const errors = validationResult(req);
 
-    if (!errors.isEmpty()) {
-      return res.status(422).json({ msg: errors.array() });
-    }
+  if (!errors.isEmpty()) {
+    return res.status(422).json({ msg: errors.array() });
+  }
   try {
     const productEdit = await ModelProduct.findByIdAndUpdate(
       { _id: req.params.id },
@@ -61,11 +83,11 @@ const editProduct = async (req, res) => {
   }
 };
 const deleteProduct = async (req, res) => {
-    const errors = validationResult(req);
+  const errors = validationResult(req);
 
-    if (!errors.isEmpty()) {
-      return res.status(422).json({ msg: errors.array() });
-    }
+  if (!errors.isEmpty()) {
+    return res.status(422).json({ msg: errors.array() });
+  }
   try {
     await ModelProduct.findByIdAndDelete({ _id: req.params.id });
     res.status(200).json({ msg: "Producto eliminado correctamente" });
@@ -76,6 +98,7 @@ const deleteProduct = async (req, res) => {
   }
 };
 module.exports = {
+  getAllProductsPagination,
   getAllProducts,
   getOneProducts,
   createProduct,
