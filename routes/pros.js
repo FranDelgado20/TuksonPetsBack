82f1bebeae140e20a7be9b33d4c12a222697cb76
SantiJaime <santij03@gmail.com>
2023-10-04 19:19:03 -0300

    modifico auth en middleware y rutas. añado verificacion para cart en users y modifico res en turns

--- routes/pros.js
+++ routes/pros.js
@@ -1,11 +1,12 @@
 const express = require("express");
 const { getAllPros, createPro } = require("../controllers/pros");
 const { check } = require("express-validator");
+const auth = require("../middleware/auth");
 const router = express.Router();
 
 router.get("/", getAllPros);
 router.post(
-  "/",
+  "/", auth(auth("admin")),
   [
     check("nombre", "Campo nombre y apellido vacío").notEmpty(),
     check("esp", "Campo especialidad vacío").notEmpty(),
