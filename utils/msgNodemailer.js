4fb6be5db99e5ae00e79555fe87b20f85a5f4e56
SantiJaime <santij03@gmail.com>
2023-10-07 12:19:55 -0300

    modifico nodemailer

--- utils/msgNodemailer.js
+++ utils/msgNodemailer.js
@@ -1,49 +1,46 @@
 const transporter = require('../helpers/nodemailer')
 
 const nuevaCuenta = async (userEmail) => {
-    await transporter.sendMail({
-        from:process.env.USER,
-        to:userEmail,
-        subject:'Bienvenido a Tukson Pets',
-        html: 
-        `
-        <h2>Bienvenido a Tukson Pets</h2>
-        <img src="http://imgfz.com/i/Er3sK8w.png" alt="Logo"   />
-        <hr/>
-        <p>Gracias por registrarte en nuestra página, aquí podras obtener diversos productos para tus mascotas ya que contamos con la mejor calidad del mercado, tambien podras consultar sobre los distintos planes que ofrecemos y solicitar turnos con nuestros profesionales. Gracias por confiar en nosotros! </p>
-        <hr/>
-        <a href="https://tukson-pets.vercel.app" >Ir a Tukson Pets</a>
-        `
-    })
+try {
+        await transporter.sendMail({
+            from:process.env.USER,
+            to:userEmail,
+            subject:'Bienvenido a Tukson Pets',
+            html: 
+            `
+            <h2>Bienvenido a Tukson Pets</h2>
+            <img src="http://imgfz.com/i/Er3sK8w.png" alt="Logo"   />
+            <hr/>
+            <p>Gracias por registrarte en nuestra página, aquí podras obtener diversos productos para tus mascotas ya que contamos con la mejor calidad del mercado, tambien podras consultar sobre los distintos planes que ofrecemos y solicitar turnos con nuestros profesionales. Gracias por confiar en nosotros! </p>
+            <hr/>
+            <a href="https://tukson-pets.vercel.app" >Ir a Tukson Pets</a>
+            `
+        })
+} catch (error) {
+    console.log(error)
+}
 }
 
 const mensajePlan = async (userEmail) => {
-    await transporter.sendMail({
-        from:process.env.USER,
-        to:userEmail,
-        subject:'Consulta sobre tu plan',
-        html: 
-        `
-        <h2>El equipo de Tukson Pets te informa:</h2>
-        <img src="http://imgfz.com/i/Er3sK8w.png" alt="Logo"   />
-        <hr/>
-        <p>Hemos visto que has solicitado mas informacion acerca de uno de nuestros planes. Proximamente nos pondremos en contacto contigo, por favor espera pacientemente. Gracias por confiar en nosotros!</p>
-        <hr/>
-        <a href="https://tukson-pets.vercel.app" >Ir a Tukson Pets</a>
-        `
-    })
+try {
+        await transporter.sendMail({
+            from:process.env.USER,
+            to:userEmail,
+            subject:'Consulta sobre tu plan',
+            html: 
+            `
+            <h2>El equipo de Tukson Pets te informa:</h2>
+            <img src="http://imgfz.com/i/Er3sK8w.png" alt="Logo"   />
+            <hr/>
+            <p>Hemos visto que has solicitado mas informacion acerca de uno de nuestros planes. Proximamente nos pondremos en contacto contigo, por favor espera pacientemente. Gracias por confiar en nosotros!</p>
+            <hr/>
+            <a href="https://tukson-pets.vercel.app" >Ir a Tukson Pets</a>
+            `
+        })
+} catch (error) {
+    console.log(error)
 }
-await new Promise((resolve, reject) => {
-    transporter.sendMail(mailData, (err, info) => {
-        if (err) {
-            console.error(err);
-            reject(err);
-        } else {
-            console.log(info);
-            resolve(info);
-        }
-    });
-});
+}
 
 module.exports ={   
     nuevaCuenta,
